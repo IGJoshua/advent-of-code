7df24baee1812fd92a97be65ed4daf93d9d06249
Joshua Suskalo <joshua@suskalo.org>
2020-11-30 22:06:44 -0600

    Initial commit

---
+++ src/advent_of_code/2020/util.clj
@@ -0,0 +1,3 @@
+(ns advent-of-code.2020.util
+  "Utility functions used accross many days.")
+
---
+++ test/advent_of_code/2020/util_test.clj
@@ -0,0 +1,4 @@
+(ns advent-of-code.2020.util-test
+  (:require [advent-of-code.2020.util :as sut]
+            [clojure.test :as t]))
+
