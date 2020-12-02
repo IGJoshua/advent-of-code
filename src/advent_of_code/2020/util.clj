db565e04012767b38f8f29148feb92a435beaaae
Joshua Suskalo <joshua@suskalo.org>
2020-12-02 00:00:10 -0600

    Add file-lines utility function

--- src/advent_of_code/2020/util.clj
+++ src/advent_of_code/2020/util.clj
@@ -1,3 +1,9 @@
 (ns advent-of-code.2020.util
-  "Utility functions used accross many days.")
+  "Utility functions used accross many days."
+  (:require
+   [clojure.java.io :as io]))
 
+(defn file-lines
+  "Returns a lazy sequence of strings for each line in the given resource."
+  [fname]
+  (line-seq (io/reader (io/resource fname))))
