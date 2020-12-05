4a134bce7d25cd742dc7b03f35e2fc8ab707e345
Joshua Suskalo <joshua@suskalo.org>
2020-12-04 23:31:20 -0600

    Add the day 5 solution

---
+++ src/advent_of_code/2020/day_5.clj
@@ -0,0 +1,56 @@
+(ns advent-of-code.2020.day-5
+  "Day five of the Advent of Code"
+  (:require
+   [advent-of-code.2020.util :refer [file-lines]]))
+
+(defn binary-search-idx
+  "Find the index of an item in a given range assuming a binary search taking the path."
+  ([path] (binary-search-idx path [0 128]))
+  ([path [mn mx]]
+   (let [mid (+ mn (/ (- mx mn) 2))]
+     (cond
+       (empty? path) mn
+       (= mn mx) mn
+       (= :lt (first path)) (binary-search-idx (rest path) [mn mid])
+       (= :gt (first path)) (binary-search-idx (rest path) [mid mx])
+       :otherwise nil))))
+
+(defn seat-num
+  "Get the seat row and column number for the given input ticket string."
+  [s]
+  (let [[_ r c] (re-matches #"([BF]{7})([RL]{3})" s)
+        r (map {\B :gt \F :lt} r)
+        c (map {\R :gt \L :lt} c)]
+    [(binary-search-idx r)
+     (binary-search-idx c [0 8])]))
+
+(defn seat-id
+  "Get the seat id of the given row and column number."
+  [[r c]]
+  (+ (* r 8) c))
+
+(defn part-1
+  "Solve part 1 of the puzzle."
+  []
+  (let [input (file-lines "day-5.txt")]
+    (transduce (comp (map seat-num)
+                     (map seat-id))
+               max
+               Integer/MIN_VALUE
+               input)))
+
+(defn part-2
+  "Solve part 2 of the puzzle."
+  []
+  (let [input (file-lines "day-5.txt")
+        seat-ids (into #{}
+                       (comp (map seat-num)
+                             (map seat-id))
+                       input)
+        min-id (reduce min seat-ids)
+        max-id (reduce max seat-ids)]
+    (first
+     (filter #(and (contains? seat-ids (inc %))
+                   (contains? seat-ids (dec %))
+                   (not (contains? seat-ids %)))
+             (range min-id max-id)))))
