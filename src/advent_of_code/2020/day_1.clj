52d0a58fd2e28f4f0c9db4d88bf065bc10c19e29
Joshua Suskalo <joshua@suskalo.org>
2020-12-10 22:50:57 -0600

    fix day 1

--- src/advent_of_code/2020/day_1.clj
+++ src/advent_of_code/2020/day_1.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code.2020.day-1
   "Day one of the Advent of Code"
   (:require
+   [advent-of-code.2020.util :refer [file-lines]]
    [clojure.java.io :as io]
    [clojure.math.combinatorics :as c]
    [clojure.string :as str]))
@@ -14,15 +15,15 @@
 (defn part-1
   "Solves the first part of the challenge"
   []
-  (let [res (map #(Integer/parseInt %))
-                 (file-lines "day-1.txt")
+  (let [res (map #(Integer/parseInt %)
+                 (file-lines "day-1.txt"))
         vals (first (numbers-sum-to 2020 2 res))]
     (apply * vals)))
 
 (defn part-2
   "Solves the second part of the challenge"
   []
-  (let [res (map (comp #(Integer/parseInt %) str/trim))
-                 (file-lines "day-1.txt")
+  (let [res (map (comp #(Integer/parseInt %) str/trim)
+                 (file-lines "day-1.txt"))
         vals (first (numbers-sum-to 2020 3 res))]
     (apply * vals)))
